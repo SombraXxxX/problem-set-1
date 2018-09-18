6b28a1f12d70ee3e43487c9612b8d1daaf287f59
Jessica Salas <jsalas@ucvts.tec.nj.us>
2018-09-18 14:20:59 -0400

    hi

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -1,13 +1,44 @@
-public class ProblemSet1 {
+/**
+ * INSTRUCTIONS.
+ * 
+ * Problem Set 1 will test your skills of printing to the console. You'll
+ * be asked to print various text, shapes, and figures, all of which is
+ * explained (and demonstrated) in detail in the README file.
+ */
+
+public class ProblemSet1 {	
+	
+	/**
+	 * Every Java program must have a <code>main</code> method, which is
+	 * where program execution begins.
+	 */
+	
 	public static void main(String[] args) {
 		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+		System.out.println("   ##    ######  ######## ########");
+		System.out.println("  #  #   #     # #        #");
+		System.out.println(	" #    #  #     # #        #");
+		System.out.println("######## ######  #        ########"); 
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       ######## ########");
 		
 		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
-		
+
+		System.out.println("  /\\     /\\     /\\");
+		System.out.println(" /  \\   /  \\   /  \\");
+		System.out.println("/    \\ /    \\ /    \\");
+		System.out.println("\\    / \\    / \\    /");
+		System.out.println(" \\  /   \\  /   \\  /");
+		System.out.println("  \\/     \\/     \\/");
 		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
-		
-		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
-		
-		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+		System.out.print("   \\\\\\\\\\ \n");
+		System.out.print("  +\"\"\"\"\" \n");	
+		System.out.print("(|  0 0  |) \n");
+		System.out.print(" |   ^   | \n");
+		System.out.print(" |  '-'  | \n");
+		System.out.print("  +-----+ \n");
+		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n H E L L O \n E       A \n L       P \n L       C \n O A P C S \n");
+		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n \"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
 	}
 }
